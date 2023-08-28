68282d75bef6f39778922d01a9ba49666c978c54
sawyehtet <sawyehtet845@gmail.com>
2023-08-28 16:34:16 +0630

    addOrderItems,getMyOrders,getOrderById

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -5,7 +5,37 @@ import Order from '../models/orderModel.js'
 // @route POST /api/orders
 // @access Private
 const addOrderItems = asyncHander(async(req,res) => {
-    res.send('add order items'); 
+    const { 
+        orderItems,
+        shippingAddress,
+        paymentMethod,
+        itemsPrice,
+        taxPrice,
+        shippingPrice,
+        totalPrice
+    } = req.body;
+
+    if(orderItems && orderItems.length === 0) {
+        res.status(400);
+        throw new Error('No order items')
+    }else {
+        const order = new Order({
+            orderItems : orderItems.map((x) => ({
+                ...x,
+                product : x._id,
+                _id : undefined
+            })),
+            user : req.user._id,
+            shippingAddress,
+            paymentMethod,
+            itemsPrice,
+            taxPrice,
+            shippingPrice,
+            totalPrice
+        });
+        const createdOrder = await order.save();
+        res.status(201).json(createdOrder);
+    }
 })
 
 
@@ -13,14 +43,21 @@ const addOrderItems = asyncHander(async(req,res) => {
 // @route POST /api/orders/myorders
 // @access Private
 const getMyOrders = asyncHander(async(req,res) => {
-    res.send('get my order items'); 
+    const orders = await Order.find({user : req.user._id});
+    res.status(200).json(orders);
 })
 
 // @desc Get order by id 
 // @route POST /api/orders/:id
 // @access Private/Admin
 const getOrderById = asyncHander(async(req,res) => {
-    res.send('get order by ID'); 
+    const order = await Order.findById(req.params.id).populate('user','name email');
+    if(order) {
+        res.status(200).json(order);
+    }else {
+        res.status(404)
+        throw new Error('Order not found');
+    }
 })
 
 // @desc Update order to paid
