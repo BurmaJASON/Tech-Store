26a638c11b44337a5fc531febe87679186b611da
sawyehtet <sawyehtet845@gmail.com>
2023-08-29 16:26:14 +0630

    creating an order

--- src/slices/orderApiSlice.js
+++ src/slices/orderApiSlice.js
@@ -2,12 +2,12 @@ import { apiSlice } from './apiSlice'
 import { ORDERS_URL } from '../constant'
 
 export const ordersApiSlice = apiSlice.injectEndpoints({
-    endpoinsts : (builder) => ({
+    endpoints : (builder) => ({
         createOrder : builder.mutation({
             query : (order) => ({
                 url : ORDERS_URL,
                 method : 'POST',
-                body : {...order}
+                body : order
             })
         })
     })
