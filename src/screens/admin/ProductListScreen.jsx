10274ef5e9b83cb83661327e928bd0edd68c3d19
sawyehtet <sawyehtet845@gmail.com>
2023-09-04 20:34:37 +0630

    createProduct in productsApiSlice

--- src/screens/admin/ProductListScreen.jsx
+++ src/screens/admin/ProductListScreen.jsx
@@ -1,12 +1,26 @@
 import { LinkContainer } from 'react-router-bootstrap'
-import { Table,Button, Row,Col } from 'react-bootstrap'
+import { Table,Button, Row,Col, Toast } from 'react-bootstrap'
 import { FaPlus, FaEdit, FaTrash } from 'react-icons/fa'
 import Message from '../../components/Message'
 import Loader from '../../components/Loader'
-import { useGetProductsQuery } from '../../slices/productsApiSlice'
+import { useGetProductsQuery, useCreateProductMutation } from '../../slices/productsApiSlice'
+import { toast } from 'react-toastify'
 
 const ProductListScreen = () => {
-    const { data, isLoading, error }= useGetProductsQuery();
+    const { data, isLoading, error, refetch }= useGetProductsQuery();
+
+    const [ createProduct, { isLoading : loadingCreate } ] = useCreateProductMutation();
+
+    const createProductHandler = async() => {
+      if(window.confirm('Are you sure to create a new product?')) {
+        try {
+          await createProduct();
+          refetch();
+        }catch (err) {
+          toast.error(err?.data?.message || err.error);
+        }
+      }
+    }
 
     const deleteHandler =(id) => {
         console.log('asdfasdf',id);
@@ -18,11 +32,12 @@ const ProductListScreen = () => {
                 <h1>Products</h1>
             </Col>
             <Col className='text-end'>
-                <Button className='btn-sm my-3'>
+                <Button className='btn-sm my-3' onClick={createProductHandler}>
                     <FaEdit />Create Product
                 </Button>
             </Col>
         </Row>   
+        { loadingCreate && <Loader/> }
         { isLoading ? <Loader/> : error? <Message variant='danger' >{error}</Message> : (
             <>
                 <Table striped hover responsive className='table-sm' >
