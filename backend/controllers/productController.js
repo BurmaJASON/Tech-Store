48d79791f306229db1b71f1d7f5f2cad1cd2c54b
sawyehtet <sawyehtet845@gmail.com>
2023-09-04 21:32:47 +0630

    updateProduct in productController

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -31,6 +31,30 @@ const createProduct = asyncHandler(async(req,res) => {
 });
 
 
+//@desc Update a product
+//@route Put /api/products/:id
+//@access Private/admin
+const updateProduct = asyncHandler(async(req,res) => {
+    const { name, price, brand, description, image, category, countInStock } = req.body;
+
+    const product = await Product.findById(req.params.id);
+    if(product) {
+        product.name = name;
+        product.price = price;
+        product.description = description;
+        product.image = image;
+        product.brand = brand;
+        product.category = category;
+        product.countInStock = countInStock;
+
+        const updatedProduct = await product.save();
+        res.json(updatedProduct);
+    }else {
+        res.status(404);
+        throw new Error('Resource not found');
+    }
+});
+
 //@desc Fetch a product 
 //@route Get /api/products/:id
 //@access Public
@@ -46,4 +70,4 @@ const getProductById = asyncHandler(async(req,res) => {
 });
 
 
-export { getProducts, getProductById, createProduct };
+export { getProducts, getProductById, createProduct, updateProduct };
