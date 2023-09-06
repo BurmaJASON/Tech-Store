aa83aac19a9fd6176f3d299266f0f3baf8f0360b
sawyehtet <sawyehtet845@gmail.com>
2023-09-06 21:08:13 +0630

    user Update in UserEditScreen

--- src/slices/userApiSlice.js
+++ src/slices/userApiSlice.js
@@ -41,8 +41,22 @@ export const usersApiSlice =  apiSlice.injectEndpoints({
                 url : `${USERS_URL}/${userId}`,
                 method : "DELETE"
             })
+        }),
+        getUserDetail : builder.query ({
+            query : (userId) => ({
+                url : `${USERS_URL}/${userId}`,
+            }),
+            keepUnusedDataFor : 5
+        }),
+        updateUser : builder.mutation({
+            query : (data) => ({
+                url : `${USERS_URL}/${data.userId}`,
+                method : "PUT",
+                body : data
+            }),
+            invalidatesTags : ['Users']
         })
     })
 });
 
-export const { useProfileMutation,useLoginMutation, useLogoutMutation, useRegisterMutation, useGetUsersQuery, useDeleteUserMutation } = usersApiSlice;
+export const { useProfileMutation,useLoginMutation, useLogoutMutation, useRegisterMutation, useGetUsersQuery, useDeleteUserMutation, useGetUserDetailQuery, useUpdateUserMutation } = usersApiSlice;
