457f2a2aded85781d6af99077bbd8ad4a06568fa
sawyehtet <sawyehtet845@gmail.com>
2023-09-08 21:51:31 +0630

    SearchBox component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,6 +7,7 @@ import logo from '../assets/logo.png'
 import { useLogoutMutation } from '../slices/userApiSlice'
 import { logout } from '../slices/authSlice'
 import { useNavigate } from 'react-router-dom'
+import SearchBox from './SearchBox'
 
 const Header = () => {
 
@@ -42,6 +43,7 @@ const Header = () => {
                 <Navbar.Toggle aria-controls='basic-navbar' />
                 <Navbar.Collapse id='basic-navbar'>
                     <Nav className="ms-auto">
+                        <SearchBox />
                         <LinkContainer to='/cart' >
                             <Nav.Link >
                                 <FaShoppingCart/>Cart 
--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -3,7 +3,7 @@ import Product from '../components/Product'
 import { useGetProductsQuery } from '../slices/productsApiSlice'
 import Loader from '../components/Loader'
 import Message from '../components/Message'
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import Paginate from '../components/Paginate'
 
 const HomeScreen = () => {
@@ -12,6 +12,7 @@ const HomeScreen = () => {
 console.log(data);
   return (
     <>
+        { keyword && <Link to='/' className='btn btn-light mb-4'>Go Back</Link> }
         { isLoading ? (
             <Loader />
         ) : error ? (
