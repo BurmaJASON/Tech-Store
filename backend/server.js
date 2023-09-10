49c2f4fbe32af4034fbfc6a5ea58af6db531cdf3
sawyehtet <sawyehtet845@gmail.com>
2023-09-10 12:28:37 +0630

    server deploy

--- backend/server.js
+++ backend/server.js
@@ -26,9 +26,6 @@ app.use(express.urlencoded({extended: true}))
 app.use(cookieParser());
 
 
-app.get('/', (req,res) => {
-    res.send('Api is running ...');
-});
 
 app.use('/api/products',productRoute);
 app.use('/api/users',userRoutes);
@@ -40,10 +37,22 @@ app.get('/api/config/paypal', (req,res) =>
 ));
 
 
-const __dirname = path.resolve(); // Set __dirname to current directory
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
+if (process.env.NODE_ENV === 'production') {
+    const __dirname = path.resolve();
+    app.use('/uploads', express.static('/var/data/uploads'));
+    app.use(express.static(path.join(__dirname, '/frontend/build')));
 
+    app.get('*', (req, res) =>
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    );
+} else {
+    const __dirname = path.resolve();
+    app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+    app.get('/', (req, res) => {
+        res.send('API is running....');
+    });
+}
 
 
 app.use(notFound);
